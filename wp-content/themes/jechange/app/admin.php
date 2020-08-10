b3bdf9def440709062f46167251cebd56aec430a
Stefan Bo <sboshnakov@gmail.com>
2020-08-10 11:06:55 +0300

    playing with rewrites

--- wp-content/themes/jechange/app/admin.php
+++ wp-content/themes/jechange/app/admin.php
@@ -78,21 +78,36 @@ add_filter( 'wp_insert_term_data', function( $data, $taxonomy, $args ) {
     }
     if($taxonomy == 'service'){
         $serviceTypeTermId = $args['acf']['field_5f2a8c8e63cdf'] ?? ''; // Service Type ACF Field
-        $serviceTypeTerm = get_term( $serviceTypeTermId, 'service_type' );
-        $data['slug'] = $serviceTypeTerm->slug . '/' . $data['slug'];
+        // $serviceTypeTerm = get_term( $serviceTypeTermId, 'service_type' );
+        $serviceTypeTermSingleSlug = get_field('single_slug', 'service_type_' . $serviceTypeTermId);
+        $data['slug'] = $serviceTypeTermSingleSlug . '/' . $data['slug'];
     }
     return $data;
 }, 99, 3);
 
 add_filter( 'wp_update_term_data', function( $data, $term_id, $taxonomy, $args ) {
     if($taxonomy == 'service_type'){
+        $mapping = [
+            'assurance' => 'assureurs',
+            'energie' => 'fournisseurs',
+            'telecom' => 'operateurs',
+            'placement' => 'banques',
+            'credit' => 'societes',
+        ];
+        // Create slug to be like energie/fournisseurs if exists in the mapping (migrated from the old site)
+        if($mapping[$data['slug']]) {
+            $data['slug'] = $data['slug'] . '/' . $mapping[$data['slug']];
+        }
+
         // TODO update slug
-        $data['slug'] = '';
+        //$data['slug'] = '';
     }
     if($taxonomy == 'service'){
         $serviceTypeTermId = $args['acf']['field_5f2a8c8e63cdf'] ?? ''; // Service Type ACF Field
-        $serviceTypeTerm = get_term( $serviceTypeTermId, 'service_type' );
-        $data['slug'] = $serviceTypeTerm->slug . '/' . sanitize_title(str_replace(',', '-', $data['name']));
+        $serviceTypeTermSingleSlug = get_field('single_slug', 'service_type_' . $serviceTypeTermId);
+        $data['slug'] = $serviceTypeTermSingleSlug . '/' . sanitize_title(str_replace(',', '-', $data['name']));
+        echo '<pre>';
+        dd($args);
     }
     return $data;
 }, 99, 4);

bc87441ac1b9373ed97083eeba1a71356e6498cc
asen <asen@grind.bg>
2020-08-27 18:57:25 +0300

    nav menu added

--- wp-content/themes/jechange/resources/assets/scripts/routes/common.js
+++ wp-content/themes/jechange/resources/assets/scripts/routes/common.js
@@ -1,6 +1,26 @@
 export default {
   init() {
-    // JavaScript to be fired on all pages
+    const mainNavItems = document.querySelectorAll('nav ul:not(.sub-menu) > li')
+
+    console.log(mainNavItems)
+
+    mainNavItems.forEach(item => {
+      item.addEventListener('mouseenter', () => {
+        openMenu(item)
+      })
+
+      item.addEventListener('mouseleave', () => {
+        openMenu(item)
+      })
+    })
+
+    function openMenu(item) {
+
+      if (!item.querySelector('.sub-menu'))
+        return;
+
+      item.querySelector('.sub-menu').classList.toggle('open')
+    }
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- wp-content/themes/jechange/resources/views/partials/header.blade.php
+++ wp-content/themes/jechange/resources/views/partials/header.blade.php
@@ -2,11 +2,13 @@
   <div class="container">
     <div class="ribbon-switch section dark-bg flex ai-center">
       <p>PARTICULIER</p>
-      @include('icons.switch')
-      <p>PROFESSIONNEL</p>
+      {{-- @include('partials.icons.switch') --}}
+      <p><a href="">PROFESSIONNEL</a></p>
     </div>
-    <div class="menu">
-      <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    <div class="menu flex">
+      <div class="brand">
+        <a href="{{ home_url('/') }}"><img src="@asset('images/logo-jechange.png')" alt=""></a>
+      </div>
       <nav class="nav-primary">
         @if (has_nav_menu('primary_navigation'))
           {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}

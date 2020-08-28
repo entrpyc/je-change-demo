72fdb3bb67dd0c3f7ae0ce12bd56268d22a67953
asen <asen@grind.bg>
2020-08-28 12:32:20 +0300

    js dynamic menu

--- wp-content/themes/jechange/resources/assets/scripts/routes/common.js
+++ wp-content/themes/jechange/resources/assets/scripts/routes/common.js
@@ -2,7 +2,7 @@ export default {
   init() {
     const mainNavItems = document.querySelectorAll('nav ul:not(.sub-menu) > li')
 
-    console.log(mainNavItems)
+    // console.log(mainNavItems)
 
     mainNavItems.forEach(item => {
       item.addEventListener('mouseenter', () => {
@@ -21,6 +21,66 @@ export default {
 
       item.querySelector('.sub-menu').classList.toggle('open')
     }
+
+    // const modelFrame = document.querySelector('[data-model-frame]')
+
+    // const modelGElements = document.querySelectorAll('[data-model-select] .g-1, [data-model-select] .g-2')
+    // // const modelG2Elements = document.querySelectorAll('[data-model-select] .g-2')
+
+
+    // modelGElements.forEach(el => {
+    //   el.classList.contains('g-1') ? modelFrame.querySelector('.g-1-parent').appendChild(el) : modelFrame.querySelector('.g-2-parent').appendChild(el)
+    // })
+
+    // const modelList = ['model-1', 'model-2', 'model-3', 'model-4']
+    // let createdModels = [];
+
+    // modelFrame.querySelectorAll('.g-1-parent > li.g-1').forEach(el => {
+    //   let key;
+    //   modelList.forEach(model => {
+    //     if(key)
+    //       return
+
+    //     key = el.classList.contains(model) ? model : false
+    //   })
+
+    //   if(!createdModels.includes(key) && key != false) {
+    //     let modelBound = document.createElement('div')
+    //     modelBound.className = key + ' parent-model'
+    //     modelFrame.querySelector('.g-1-parent').appendChild(modelBound)
+
+    //     createdModels.push(key)
+    //   }
+
+    //   if(key) {
+    //     modelFrame.querySelector(`.g-1-parent > .parent-model.${key}`).appendChild(el)
+    //   }
+    // })
+
+    // modelFrame.querySelectorAll('.g-2-parent > li.g-2').forEach(el => {
+    //   let key;
+    //   modelList.forEach(model => {
+    //     if(key)
+    //       return
+
+    //     key = el.classList.contains(model) ? model : false
+    //   })
+
+    //   console.log(key)
+
+    //   if(!createdModels.includes(key) && key != false) {
+    //     let modelBound = document.createElement('div')
+    //     modelBound.className = key + ' parent-model'
+    //     modelFrame.querySelector('.g-2-parent').appendChild(modelBound)
+
+    //     createdModels.push(key)
+    //   }
+
+    //   if(key) {
+    //     modelFrame.querySelector(`.g-2-parent > .parent-model.${key}`).appendChild(el)
+    //   }
+    // })
+
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- wp-content/themes/jechange/resources/views/partials/header.blade.php
+++ wp-content/themes/jechange/resources/views/partials/header.blade.php
@@ -2,14 +2,23 @@
   <div class="container">
     <div class="ribbon-switch section dark-bg flex ai-center">
       <p>PARTICULIER</p>
-      {{-- @include('partials.icons.switch') --}}
+      {{-- <img src="@asset('images/svg-icons/switch.svg')" alt=""> --}}
       <p><a href="">PROFESSIONNEL</a></p>
     </div>
     <div class="menu flex">
       <div class="brand">
         <a href="{{ home_url('/') }}"><img src="@asset('images/logo-jechange.png')" alt=""></a>
       </div>
-      <nav class="nav-primary">
+      <div class="flex" data-model-frame style="display:none">
+        <div class="g-1-parent flex flex-column"></div>
+        <div class="g-2-parent flex flex-column"></div>
+      </div>
+      {{-- <div data-model-style>
+        <div class="model-1"></div>
+        <div class="model-2"></div>
+        <div class="model-3"></div>
+      </div> --}}
+      <nav class="nav-primary" data-model-select>
         @if (has_nav_menu('primary_navigation'))
           {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
         @endif

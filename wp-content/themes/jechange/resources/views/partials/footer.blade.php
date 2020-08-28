367d1ff7fdd5d0746969780677451b8909f4339a
asen <asen@grind.bg>
2020-08-28 17:57:47 +0300

    added footer

--- wp-content/themes/jechange/resources/views/partials/footer.blade.php
+++ wp-content/themes/jechange/resources/views/partials/footer.blade.php
@@ -1,3 +1,37 @@
 <footer>
-
+  <div class="container">
+    <div class="social-icons flex">
+      <div class="i"><img src="@asset('images/svg-icons/social-fb.svg')" alt=""></div>
+      <div class="i"><img src="@asset('images/svg-icons/social-tw.svg')" alt=""></div>
+      <div class="i"><img src="@asset('images/svg-icons/social-ig.svg')" alt=""></div>
+    </div>
+    <div class="links">
+      <ul class="flex flex-wrap">
+        <li>
+          <a href="https://groupe.jechange.fr/">Qui sommes-nous</a>
+        </li>
+        <li>
+          <a href="https://groupe.jechange.fr/jobs/">Recrutement</a>
+        </li>
+        <li>
+          <a href="/presse">Presse</a>
+        </li>
+        <li>
+          <a href="https://www.jechange.fr/contact">Contact</a>
+        </li>
+        <li>
+          <a href="https://www.jechange.fr/mentions">Mentions</a>
+        </li>
+        <li>
+          <a href="https://www.jechange.fr/cgu">Conditions générales d’utilisation du site JeChange</a>
+        </li>
+        <li>
+          <a href="https://www.jechange.fr/partenaires">Prestataires</a>
+        </li>
+        <li>
+          <a href="/partenariats">Partenariats</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </footer>

1bca1189c288804616a3d34978105231aa84dacd
unknown <georgi@grind.bg>
2020-08-28 16:32:04 +0300

    images for blocks

--- wp-content/themes/jechange/resources/functions.php
+++ wp-content/themes/jechange/resources/functions.php
@@ -93,3 +93,55 @@ Container::getInstance()
             'view' => require dirname(__DIR__).'/config/view.php',
         ]);
     }, true);
+
+
+/* Hover Module Effect */
+function my_acf_admin_head() {
+    $siteURL = get_site_url();
+    ?>
+    <style type="text/css">
+        .imagePreview { position:absolute; right:100%; top:0px; z-index:999999; border:1px solid #f2f2f2; box-shadow:0px 0px 3px #b6b6b6; background-color:#fff; padding:20px;}
+        .imagePreview img { width:460px; height:auto; display:block; }
+        .acf-tooltip li:hover { background-color:#0074a9; }
+        .module-preview {
+            position: absolute;
+            right: 100%;
+            top: 0px;
+            z-index: 1;
+            background-color: #fff;
+            padding: 20px;
+        }
+        .module-preview img {
+            width: 500px;
+        }
+
+        .acf-tooltip li:hover {
+            background-color:#0074a9;
+        }
+    </style>
+    <script>
+        jQuery(document).ready(function($) {
+            $('a[data-name=add-layout]').click(function(){
+                waitForEl('.acf-tooltip li', function() {
+                    $('.acf-tooltip li a').hover(function(){
+                        imageTP = $(this).attr('data-layout');
+                        $('.acf-tooltip').append('<div class="imagePreview"><img src="<?php echo $siteURL; ?>/wp-content/themes/jechange/modules/images/' + imageTP + '.png"></div>');
+                    }, function(){
+                        $('.imagePreview').remove();
+                    });
+                });
+            })
+            var waitForEl = function(selector, callback) {
+                if (jQuery(selector).length) {
+                    callback();
+                } else {
+                    setTimeout(function() {
+                        waitForEl(selector, callback);
+                    }, 100);
+                }
+            };
+        })
+    </script>
+    <?php
+}
+add_action('acf/input/admin_head', 'my_acf_admin_head');
